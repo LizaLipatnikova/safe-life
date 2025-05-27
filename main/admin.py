e68e77a45ced6de4b017c20772afe3058ba68cee
LizaLipatnikova <lizalipatnikova1999@gmail.com>
2025-05-27 13:46:59 +0300

    Добавлена модель новостей

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from main.models import Post
 
-# Register your models here.
+# Регистрируем модель на административной панели с помощью декоратора
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    # Какие поля отображать в списке всех объектов модели
+    list_display = ["title", "created_at",]
--- main/models.py
+++ main/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from imagekit.models.fields import ImageSpecField
+from imagekit.processors import ResizeToFit
 
-# Create your models here.
+# Модель новостей на главной странице
+class Post(models.Model):
+    title = models.CharField("Заголовок", max_length=120)
+    desc = models.TextField("Описание")
+    image = models.ImageField("Изображение", upload_to="image_posts/") 
+
+    # Преобразуем изображение к нужному размеру и формату
+    # Данное поле не будет в базе данных, оно просто автоматические создаст в папке медиа-файлов нужное изображение
+    format_image = ImageSpecField([ResizeToFit(width=500), ], source="image", format="PNG")
+
+    # Дата создания. Поле заполняется автоматически
+    created_at = models.DateTimeField("Дата добавления", auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        # Сортируем по самым свежим новостям
+        ordering = ('-created_at',)
+        verbose_name = "Пост"
+        verbose_name_plural = "Посты"
