f70eebedc36fa66dd2c820fe8ad4f005c8084a7b
LizaLipatnikova <lizalipatnikova1999@gmail.com>
2025-05-28 17:36:27 +0300

    Динамическое определение активной страницы

--- main/views.py
+++ main/views.py
@@ -1,8 +1,21 @@
 from django.views.generic import ListView
 from main.models import Post
 
+# Класс-примесь, для указания дополнительной логики некоторым представлениям
+# Данный класс добавляет в контекст шаблона индентификатор, который сообщаем меню сайта, какая страница активна
+class MenuMixin:
+    id_page = ""
+
+    # Переопределяем метод, которые будет в других классах, добавляя дополнительную переменную
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["id_active_page"] = self.id_page
+        return context
+    
+
 # Главная страница
-class IndexView(ListView):
+class IndexView(MenuMixin, ListView):
+    id_page = "index"
     model = Post
     context_object_name = "posts"
     template_name = "index.html"
---
+++ static/js/menu.js
@@ -0,0 +1,14 @@
+function bootstrapMenu() {
+    const CLS_ACTIVE = "active";
+    const idActivePage = document.querySelector("#active_page");
+    if (!idActivePage) {
+        console.error("Не удалось инцилизировать меню. Не найдена метка активный страницы");
+        return;
+    }
+    const menuItems = document.querySelectorAll(".main-menu-link");
+    menuItems.forEach(item => {
+        if (item.id === idActivePage.textContent) item.classList.add(CLS_ACTIVE);
+    })
+}
+
+bootstrapMenu();
