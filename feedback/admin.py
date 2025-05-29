630e19c6f23070f68508950c2aad1ff7a4d39c72
LizaLipatnikova <lizalipatnikova1999@gmail.com>
2025-05-30 02:33:12 +0300

    Добавлена модель отзывов

--- feedback/admin.py
+++ feedback/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from feedback.models import Feedback
 
-# Register your models here.
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ["user__username", "created_at", ]
+
+    # Программное поле user__username
+    def user__username(self, obj):
+        return obj.user.username
+    
+    user__username.short_description = "Пользователь" # Подпись поля
--- feedback/models.py
+++ feedback/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Модель отзыва
+class Feedback(models.Model):
+    user = models.ForeignKey(
+        User,
+        verbose_name="Отзыв",
+        on_delete=models.CASCADE,
+        related_name="feedbacks"
+    )
+    text = models.TextField("Содержание")
+    created_at = models.DateField("Дата написания", auto_now_add=True)
+
+    def __str__(self):
+        return f"Отзыв {self.user.username} от {self.created_at}"
+
+    class Meta:
+        # Сортируем по самым свежим отзывам
+        ordering = ('-created_at',)
+        verbose_name = "Отзыв"
+        verbose_name_plural = "Отзывы"
