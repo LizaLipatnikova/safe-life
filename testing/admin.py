e4f8a61476eee0592a617f328f580b6f885114bb
LizaLipatnikova <lizalipatnikova1999@gmail.com>
2025-05-31 15:12:31 +0300

    Добавлены модели вопроса и ответов для онлайн теста

--- testing/admin.py
+++ testing/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from testing.models import Question, Answer
 
-# Register your models here.
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ["question", "weight", ]
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ["question__question", "answer", "is_correct",]
+
+    def question__question(self, obj):
+        return obj.question.question
+
+    question__question.short_description = "Вопрос"
