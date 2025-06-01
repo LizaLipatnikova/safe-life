78b430b736c742390e493a96aeec346164daa06a
LizaLipatnikova <lizalipatnikova1999@gmail.com>
2025-06-01 15:26:25 +0300

    Добавлены представления для аутентификации

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from user.views import AuthWarningView
+from user.views import AuthWarningView, UserLoginView, RegistrationView, AccountView, UserLogoutView
 
 urlpatterns = [
     path('warning/', AuthWarningView.as_view(), name="auth_warning"),
+    path('login/', UserLoginView.as_view(), name="login"),
+    path('registration/', RegistrationView.as_view(), name="registration"),
+    path('account/', AccountView.as_view(), name="account"),
+    path('logout/', UserLogoutView.as_view(), name="logout"),
 ]
--- user/views.py
+++ user/views.py
@@ -1,5 +1,27 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
 
 # Страница, сообщающая пользователя о необходимости атуентификации
 class AuthWarningView(TemplateView):
     template_name = "auth_warning.html"
+
+# Авторизация
+class UserLoginView(LoginView):
+    template_name = "login.html"
+    success_url = reverse_lazy("account")
+
+# Регистрация
+class RegistrationView(CreateView):
+    model = User
+    template_name = "registration.html"
+    success_url = reverse_lazy("login")
+
+# Личный кабинет
+class AccountView(TemplateView):
+    template_name = "account.html"
+
+# Выход из аккаунта. LogoutView не имеет специфических настроек, поэтому класс пустой
+class UserLogoutView(LogoutView):
+    pass
